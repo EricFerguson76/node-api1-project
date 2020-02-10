9e2bdefe194a982e3c40f820a67d87380dc95365
Eric Ferguson <turtle0888@gmail.com>
2020-02-10 16:27:29 -0600

    Add GET request to api

--- index.js
+++ index.js
@@ -6,9 +6,61 @@ const server = express();
 
 server.use(express.json());
 
+//GET
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: 'The users information could not be retrieved.'
+      });
+    });
+});
+
+//GET USER BY ID
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  Users.findById(id)
+    .then(found => {
+      if (found) {
+        res.status(200).json(found);
+      } else {
+        res
+          .status(400)
+          .json({ message: 'The user with the specified ID does not exist' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: 'The user infomation could not be retrieved' });
+    });
+});
+
 //POST
 server.post('/api/users', (req, res) => {
   const userData = req.body;
+
+  Users.insert(userData)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(() => {
+      if (userData !== 'name' && 'bio') {
+        res
+          .status(400)
+          .json({ errorMessage: 'Please provide name and bio for the user' });
+      } else {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database'
+        });
+      }
+    });
 });
 
 const port = 8000;
