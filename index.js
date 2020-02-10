e46f9f099373f72f45559e11ab0c8afbf70d7900
Eric Ferguson <turtle0888@gmail.com>
2020-02-10 17:20:39 -0600

    Add PUT request to api

--- index.js
+++ index.js
@@ -63,5 +63,56 @@ server.post('/api/users', (req, res) => {
     });
 });
 
+//DELETE
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  Users.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res
+          .status(200)
+          .json({ message: 'This user was deleted successfully', deleted });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: 'The user could not be removed' });
+    });
+});
+
+//PUT
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user' });
+  } else {
+    Users.update(id, req.body)
+      .then(updated => {
+        if (updated) {
+          res.status(200).json({ message: 'Updated ${req.body}' });
+        } else {
+          res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist' });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({
+            errorMessage: 'The user information could not be modified.'
+          });
+      });
+  }
+});
+
 const port = 8000;
 server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`));
