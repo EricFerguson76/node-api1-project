dcbd51639ae683ec7f2b8ad2a0e9de72ab6fd71f
Eric Ferguson <turtle0888@gmail.com>
2020-02-10 15:51:26 -0600

    Set up server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express');
+
+const Users = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+//POST
+server.post('/api/users', (req, res) => {
+  const userData = req.body;
+});
+
+const port = 8000;
+server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`));
